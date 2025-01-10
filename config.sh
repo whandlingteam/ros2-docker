c58f8de5505f1d475db33a3e71a71670f561bd5d
HandlingTeam <whandlingteam@gmail.com>
2025-01-11 01:51:05 +0900

    イメージ名を若干変更

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 # コンテナ名。ホスト側から作業ディレクトリ名として設定され、terminatorのウィンドウ名にも反映される。
-CONTAINER_NAME=ros2-foxy-bridge
+CONTAINER_NAME=ros2-foxy
 
 # イメージの基本設定
 # ベースイメージの設定。タグは https://hub.docker.com/r/nvidia/opengl/tags?name=base-ubuntu から調べる
 BASE_IMAGE="base-ubuntu20.04"
 ROS_DISTRO=foxy
-IMAGE_NAME=ros2_${ROS_DISTRO}
+IMAGE_NAME=ros2-${ROS_DISTRO}
 
 # ホスト側PCの設定
 DEV_WS=${HOME}/dev_ws # terminatorの設定ファイル置き場
